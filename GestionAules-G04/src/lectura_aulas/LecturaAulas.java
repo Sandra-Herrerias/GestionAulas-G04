36e814f7ffa7825d6869c8a9cb264f4d88c2a923
Sandra <s.herreriasrodriguez@gmail.com>
2021-03-23 21:27:07 +0100

    Funcion "AddRecord" añadida

--- GestionAules-G04/src/lectura_aulas/LecturaAulas.java
+++ GestionAules-G04/src/lectura_aulas/LecturaAulas.java
@@ -1,6 +1,7 @@
 package lectura_aulas;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 /*
@@ -8,8 +9,11 @@ import java.util.Scanner;
  */
 public class LecturaAulas {
 
-    public static void main(String[] args) {
+    public static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        
+        AddRecord();
         leer_archivo();
     }
 
@@ -19,7 +23,7 @@ public class LecturaAulas {
     public static void leer_archivo() {
         String classroom;
         String id_aula, descripcio_aula;
-        int capacitat_aula,num_pc;
+        int capacitat_aula, num_pc;
         boolean pc_aula, projector_aula, insonoritzada_aula;
         //File: clase que me guarda el fichero
         //classrooms: variable que me apunta al fichero de texto que quiero leer
@@ -34,7 +38,7 @@ public class LecturaAulas {
                 classroom = sc.nextLine();
                 clase = classroom.split(",");//separamos String[] por comas para poder tratar cada valor por separado
                 if (!first_see) {
-                    //Assignamos el valor de la pos[x] del vector clase[] a una variable determinada
+                    //Asignamos el valor de la pos[x] del vector clase[] a una variable determinada
                     id_aula = clase[0];
                     descripcio_aula = clase[1];
                     capacitat_aula = Integer.parseInt(clase[2]);
@@ -59,5 +63,46 @@ public class LecturaAulas {
             //Exception e: Cualquier tipo de error (no especifico) lo detecta
             System.out.println("No se lee el archivo.");//mostramos mensaje de error
         }
+    }
+
+    /**
+     * Funcion que permite crear a la usuaria una nueva entrada en el fichero
+     */
+    public static void AddRecord() {
+
+        File fichero = new File("files/classrooms.csv");
+
+        try {
+            // El true al final indica que escribiremos al final del fichero añadiendo texto
+            FileWriter writer = new FileWriter(fichero, true);
+            
+            String id_aula, descripcio_aula;
+            int capacitat_aula, num_pc;
+            boolean pc_aula, projector_aula, insonoritzada_aula;
+            
+            System.out.println("Introduce el id de el Aula: ");
+            id_aula = sc.nextLine();
+            System.out.println("Introduce la descripcion de el Aula: ");
+            descripcio_aula = sc.nextLine();
+            System.out.println("Introduce la capacidad de el Aula: ");
+            capacitat_aula = sc.nextInt();
+            System.out.println("Introduce si el Aula tiene ordenadores: ");
+            pc_aula = sc.nextBoolean();
+            System.out.println("Introduce el numero de ordenadores que tiene el Aula: ");
+            num_pc = sc.nextInt();
+            System.out.println("Introduce si el Aula tiene proyector: ");
+            projector_aula = sc.nextBoolean();
+            System.out.println("Introduce el Aula está insonorizada: ");
+            insonoritzada_aula = sc.nextBoolean();
+            
+            writer.write(id_aula+","+descripcio_aula+","+capacitat_aula+","+
+                    pc_aula+","+num_pc+","+projector_aula+","+insonoritzada_aula + "\n");
+            writer.flush();//limpia la memoria del writer
+            
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al crear/escribir en el fichero");
+        }
+
     }
 }
